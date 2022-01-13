8b755581abeb7553e56066890fe5259fb49cea04
Gabriel Barbosa Da Conceição <gabriel.bc.dev@hotmail.com>
2022-01-13 09:40:19 -0300

    Initial commit

---
+++ Aluno.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace mediaDoAluno
+{
+    public class Aluno
+    {
+        public string nomeAluno { get; set; }
+        public double nota1 { get; set; }
+        public double nota2 { get; set; }
+        public  double total { get; set; }
+        public double media { get; set; }
+
+        public string NomeAluno
+        {
+            get { return nomeAluno; }
+            set { nomeAluno = Console.ReadLine(); }
+        }
+
+        public void SomarNotas()
+        {
+            total = nota1 + nota2;
+            Console.WriteLine($"\nO soma das notas do aluno {nomeAluno}, é [{total.ToString("F2", CultureInfo.InvariantCulture)}].");
+        }
+
+        public void Media()
+        {
+            media = (nota1 + nota2) / 2.0;
+            Console.WriteLine($"\nA média das notas do aluno {nomeAluno}, foi [{media.ToString("F2", CultureInfo.InvariantCulture)}].");
+        }
+
+        public void PassouDeAno()
+        {
+            if (media >= 8.0 && media <= 10.0)
+            {
+                Console.WriteLine($"\nParabéns! O aluno {nomeAluno} passou de ano diréto. ");
+            }
+            else if (media >= 6.0 && media < 8.0)
+            {
+                Console.WriteLine($"\nO aluno {nomeAluno} ficou de recuperação. ");
+            }
+            else
+            {
+                Console.WriteLine($"\nO aluno {nomeAluno} reprovou.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+namespace mediaDoAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextInfo text = CultureInfo.CurrentCulture.TextInfo;
+
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Digite o nome do aluno: ");
+            aluno.nomeAluno = Console.ReadLine();
+            aluno.nomeAluno = text.ToTitleCase(aluno.nomeAluno);
+
+            Console.WriteLine("Digite a primeira nota: ");
+            aluno.nota1 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite a segunda nota: ");
+            aluno.nota2 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            aluno.SomarNotas();
+
+            aluno.Media();
+
+            aluno.PassouDeAno(); 
+        }
+    }
+}
